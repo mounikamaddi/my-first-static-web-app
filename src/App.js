53d7d395e90ca3a8d96d745b95b01b9e5b0b1985
mounikamaddi <123763049+mounikamaddi@users.noreply.github.com>
2023-09-11 19:39:42 -0700

    Update App.js
    commit-1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import './App.css';
+import Lab1 from './Lab1.jpg';
 import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>
+            <p> I did my homewor.</p>
+    <img src={Lab1}/>
+    </div>;
 }
 
 export default App;
